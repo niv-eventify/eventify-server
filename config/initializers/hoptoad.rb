7ddf7319e8f9e61a1dbfe3597aa4333355ce4d9a
Vitaly Kushner <vitaly@astrails.com>
2009-11-16 19:30:05 +0200

    hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,4 +1,4 @@
 HoptoadNotifier.configure do |config|
-  config.api_key = 'eventify-hoptoad-pass'
+  config.api_key = 'eaa2a9c8469acaa53e2734ca5d9dc3d8'
   config.environment_filters << 'rack-bug.*'
 end
