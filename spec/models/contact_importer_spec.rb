8d3c9f6589af8b89d9a4adfc8d5424d16fab54f2
Boris Nadion <boris@astrails.com>
2009-11-24 13:03:08 +0200

    importer validations

--- spec/models/contact_importer_spec.rb
+++ spec/models/contact_importer_spec.rb
@@ -14,4 +14,27 @@ describe ContactImporter do
       @contact_importer.errors.on(:contact_source).should_not be_blank
     end
   end
+
+  describe "import" do
+    describe "validation" do
+      it "should validate user/password for not csv types" do
+        (ContactImporter::SOURCES.keys - ["csv"]).each do |s|
+          @contact_importer.contact_source = s
+          @contact_importer.validate_importing = true
+          @contact_importer.should_not be_valid
+          [:password, :username].each do |k|
+            @contact_importer.errors.on(k).should_not be_blank
+          end
+        end
+      end
+      it "should validate file on csv" do
+        @contact_importer.contact_source = "csv"
+        @contact_importer.validate_importing = true
+        @contact_importer.should_not be_valid
+        @contact_importer.errors.on(:csv).should_not be_blank
+      end
+    end
+
+    # describe "importer"
+  end
 end
