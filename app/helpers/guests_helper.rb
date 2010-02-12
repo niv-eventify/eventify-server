d4e0fa345b4777ab4d7288e997e6dfba3c83b049
Boris Nadion <boris@astrails.com>
2010-02-12 17:03:57 +0200

    new guests automations

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -17,7 +17,7 @@ module GuestsHelper
 
   def guest_remote_checkbox(attribute, event, guest)
     form_remote_for :guest, guest, :url => event_guest_path(event, guest), :method => :put do |f|
-      haml_concat f.check_box(attribute, :class => "input-check", :onchange => "jQuery(this).parents('form').get(0).onsubmit()")
+      haml_concat f.check_box(attribute, :class => "input-check", :onchange => "jQuery(this).parents('form').get(0).onsubmit()", :id => "#{dom_id(guest)})_#{attribute}")
     end
   end
 
@@ -27,5 +27,26 @@ module GuestsHelper
     else
       guest_remote_checkbox(attribute, event, guest)
     end
+  end
+
+  def change_observers
+    javascript_tag <<-JAVASCRIPT
+      jQuery("#guest_email").keyup(function(){
+        if ("" != jQuery(this).val()) {
+          jQuery("#guest_send_email").attr("checked", "checked").get(0)._replaced.removeClass().addClass("checkboxAreaChecked");
+        }
+        else {
+          jQuery("#guest_send_email").removeAttr("checked").get(0)._replaced.removeClass().addClass("checkboxArea");
+        }
+      });
+      jQuery("#guest_mobile_phone").keyup(function(){
+        if ("" != jQuery(this).val()) {
+          jQuery("#guest_send_sms").attr("checked", "checked").get(0)._replaced.removeClass().addClass("checkboxAreaChecked");
+        }
+        else {
+          jQuery("#guest_send_sms").removeAttr("checked").get(0)._replaced.removeClass().addClass("checkboxArea");
+        }
+      });
+    JAVASCRIPT
   end
 end
