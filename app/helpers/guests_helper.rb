ae1d84947896502d8c463217a47c01ab7bb1e93b
Boris Nadion <boris@astrails.com>
2010-02-14 16:37:25 +0200

    guests search

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -12,7 +12,7 @@ module GuestsHelper
   def link_to_remove_guest(event, guest)
     return nil if guest.invited?
     link_to_remote "del", :url => event_guest_path(event, guest), :method => :delete, 
-      :confirm => _("Are you sure?"), :html => {:class => "bin"}, :before => "$('##{dom_id(guest)}').hide()"
+      :confirm => _("Are you sure?"), :html => {:class => "bin"}, :before => "$('tr##{dom_id(guest)}').remove();jQuery.fn.reload_search();"
   end
 
   def guest_remote_checkbox(attribute, event, guest)
@@ -32,7 +32,7 @@ module GuestsHelper
   def guest_remote_rsvp(event, guest)
     form_remote_for :guest, guest, :builder => NoLabelFormBuilder::Builder, :url => event_guest_path(event, guest), :method => :put do |f|
       haml_concat f.input(:rsvp, :as => :select, :collection => rsvp_kinds_for_select,
-        :input_html => {:id => "#{dom_id(guest)}_rsvp", :class => "rspv_select",
+        :input_html => {:id => "#{dom_id(guest)}_rsvp", :class => "rspv_select", :guest_id => guest.id.to_s,
         :onchange => "jQuery.fn.rsvp_update_color(this);jQuery(this).parents('form').get(0).onsubmit();"}, :wrapper_html => {:class => "guest_select_rsvp", :id => ""})
     end    
   end
@@ -76,9 +76,54 @@ module GuestsHelper
             var color_index = parseInt(s.val() || -1) + 1;
             s.prev().css({color: colors[color_index]});
           };
+          var guests_texts = {};
+          jQuery.fn.reload_search = function() {
+            guests_texts = {};
+            jQuery("tr.guest").each(function(){
+              var t = jQuery(this);
+              var text = t.find("td.t-col-1 label[for='name-1']").html();
+              text += t.find("td.t-col-2 a").html() + " ";
+              text += t.find("td.t-col-3 div.cell-bg").html();
+              guests_texts[jQuery(this).attr("id")] = text;
+            });
+          }
+          jQuery.fn.rsvp_filter_by = function(sel_index) {
+            if (!sel_index) {
+              jQuery("tr.guest").show();
+            }
+            else {
+              jQuery("tr.guest").hide();
+              jQuery(".rspv_select[selectedIndex=" + sel_index+ "]").each(function(){
+                jQuery(this).parents("tr.guest").show();
+              })
+            }
+          }
           jQuery(".rspv_select").select_skin();
           jQuery(".rspv_select").each(function(){
             jQuery.fn.rsvp_update_color(this);
+          });
+          jQuery("#filter_all").click(function(){
+            jQuery.fn.rsvp_filter_by(null);
+          });
+          jQuery("#filter_yes").click(function(){
+            jQuery.fn.rsvp_filter_by(1 + 1);
+          });
+          jQuery("#filter_no").click(function(){
+            jQuery.fn.rsvp_filter_by(0 + 1);
+          });
+          jQuery("#filter_maybe").click(function(){
+            jQuery.fn.rsvp_filter_by(2 + 1);
+          });
+          jQuery.fn.reload_search();
+          jQuery("#search_guests").keyup(function(){
+            var pattern = jQuery("#search_guests").val();
+            jQuery.each(guests_texts, function(k, v){
+              jQuery("#" + k)[v.match(pattern) ? "show" : "hide"]();
+            });
+          });
+          jQuery("#cancel_search").click(function(){
+            jQuery("tr.guest").show();
+            jQuery("#search_guests").val("");
           });
       });
     JAVASCRIPT
