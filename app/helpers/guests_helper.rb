52343a283855d688b699f9cd9dad0495171d0482
Boris Nadion <boris@astrails.com>
2010-02-14 15:51:58 +0200

    colors magic

--- app/helpers/guests_helper.rb
+++ app/helpers/guests_helper.rb
@@ -33,7 +33,7 @@ module GuestsHelper
     form_remote_for :guest, guest, :builder => NoLabelFormBuilder::Builder, :url => event_guest_path(event, guest), :method => :put do |f|
       haml_concat f.input(:rsvp, :as => :select, :collection => rsvp_kinds_for_select,
         :input_html => {:id => "#{dom_id(guest)}_rsvp", :class => "rspv_select",
-        :onchange => "jQuery(this).parents('form').get(0).onsubmit()"}, :wrapper_html => {:class => "guest_select_rsvp", :id => ""})
+        :onchange => "jQuery.fn.updateColor(this);jQuery(this).parents('form').get(0).onsubmit();"}, :wrapper_html => {:class => "guest_select_rsvp", :id => ""})
     end    
   end
 
@@ -70,7 +70,16 @@ module GuestsHelper
     javascript_include_tag("jquery.select_skin") +
     javascript_tag(<<-JAVASCRIPT
       jQuery(document).ready(function() {
+          jQuery.fn.updateColor = function(el) {
+            var s = jQuery(el);
+            var colors = ["#000", "#e82303", "#28b20a", "#c6770d"]
+            var color_index = parseInt(s.val() || -1) + 1;
+            s.prev().css({color: colors[color_index]});
+          };
           jQuery(".rspv_select").select_skin();
+          jQuery(".rspv_select").each(function(){
+            jQuery.fn.updateColor(this);
+          });
       });
     JAVASCRIPT
     )
