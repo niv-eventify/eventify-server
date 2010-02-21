7ebad9eae86996a0966f206f50730321fd89d59c
Boris Nadion <boris@astrails.com>
2010-02-21 14:51:40 +0200

    more specs

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -2,9 +2,42 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe PaymentsController do
 
-  #Delete this example and add some real ones
-  it "should use PaymentsController" do
-    controller.should be_an_instance_of(PaymentsController)
+  setup :activate_authlogic
+
+
+  describe "not logged in user" do
+    describe_action(:create) do
+      before(:each) do
+        @params = {:event_id => 1}
+      end
+      it_should_require_login
+    end
+  end
+
+  describe "owner" do
+    integrate_views
+    
+    before(:each) do
+      @user = Factory.create(:active_user)
+      UserSession.create(@user)
+      @event = stub_model(Event)
+      controller.current_user.events.stub!(:find).and_return(@event)
+    end
+
+    it "should redirect to summary page" do
+      @event.stub(:payment_required?).and_return(false)
+      @event.stub(:payments).and_return([])
+      get :index, :event_id => @event
+      response.should redirect_to("/events/#{@event.id}/summary")
+    end
+
+    it "should render required payments" do
+      @event.stub(:payment_required?).and_return(true)
+      @event.stub(:payments).and_return([])
+      get :index, :event_id => @event
+      response.should be_success
+      response.should render_template("index")
+    end
   end
 
 end
--- spec/controllers/summary_controller_spec.rb
+++ spec/controllers/summary_controller_spec.rb
@@ -2,9 +2,33 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe SummaryController do
 
-  #Delete this example and add some real ones
-  it "should use SummaryController" do
-    controller.should be_an_instance_of(SummaryController)
+  setup :activate_authlogic
+
+
+  describe "not logged in user" do
+    describe_action(:show) do
+      before(:each) do
+        @params = {:event_id => 1}
+      end
+      it_should_require_login
+    end
+  end
+
+  describe "owner" do
+    integrate_views
+    
+    before(:each) do
+      @user = Factory.create(:active_user)
+      UserSession.create(@user)
+      @event = stub_model(Event)
+    end
+
+    it "should render summary" do
+      controller.current_user.events.stub!(:find).and_return(@event)
+      @event.should_receive(:invitations_to_send_counts).and_return({:email => 2, :sms => 3,:total => 5})
+      get :show, :event_id => @event
+      response.should be_success
+    end
   end
 
 end
