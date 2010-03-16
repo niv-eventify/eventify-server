e3187975666071cccf7534841aaf75199a058f50
Boris Nadion <boris@astrails.com>
2010-03-16 16:05:40 +0200

    reminders specs and fixes

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,38 +1,39 @@
 class Reminder < ActiveRecord::Base
   belongs_to :event
 
-  BEFORE_UNITS  = HashWithIndifferentAccess.new(
-    "hours" => N_("Hours")
-    "days" => N_("Days"),
-    "months" => N_("Months"),
-  )
+  BEFORE_UNITS  = ActiveSupport::OrderedHash.new do |h|
+    h["hours"]  = N_("Hours")
+    h["days"]   = N_("Days")
+    h["months"] = N_("Months")
+  end
 
   attr_accessible :to_yes, :to_no, :to_may_be, :to_not_responded, :by_email, :by_sms, 
     :email_subject, :email_body, :sms_message, :before_units, :before_value
 
-  named_scope :pending, :conditions => ["reminders.reminder_sent_at IS NULL AND reminders.send_reminder_at < ?", Time.now.utc]
+  named_scope :pending, :conditions => ["reminders.reminder_sent_at IS NULL AND reminders.send_reminder_at > ?", Time.now.utc]
   named_scope :with_event, :include => :event
 
   before_validation :set_send_date
   def set_send_date
     self.before_units = BEFORE_UNITS.keys.member?(before_units) ? before_units : "days"
-    self.send_at = event.starting_at - before_value.to_i.send(before_units)
+    self.send_reminder_at = event.starting_at - (before_value || 0).to_i.send(before_units)
   end
 
   validates_presence_of :before_units, :before_value
   validates_presence_of :email_subject, :email_body, :if => :by_email?
-  validates_length_of   :email_subject, :limit => 255, :if => :by_email?
-  validates_length_of   :email_body, :limit => 2048, :if => :by_email?
+  validates_length_of   :email_subject, :within => 2..255, :if => :by_email?
+  validates_length_of   :email_body, :within => 2..255, :if => :by_email?
   validates_presence_of :sms_message, :if => :by_sms
 
   def validate
-    errors.add(:before_value, _("should be in a future")) if reminder_sent_at.nil?  && (!send_at || send_at >= event.starting_at)
+    errors.add(:before_value, _("should be in a future")) if reminder_sent_at.nil?  && (!send_reminder_at || send_reminder_at >= event.starting_at)
     errors.add(:by_email, s_("...mail or sms|choose a delivery method")) if !by_email? && !by_sms?
     errors.add(:to_yes, s_("can't be blank")) if !to_yes? && !to_no? && !to_may_be? && !to_not_responded?
   end
 
   def deliver!
     # TODO send a reminder
+    # fill reminder log
   end
 
   def self.send_reminders
--- spec/models/reminder_spec.rb
+++ spec/models/reminder_spec.rb
@@ -1,13 +1,78 @@
 require 'spec_helper'
 
 describe Reminder do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
+  describe "validations" do
+    before(:each) do
+      @event = Factory.create(:event)
+      @reminder = @event.reminders.build
+    end
+
+    it "should validate presense of before_units/before_value" do
+      @reminder.should_not be_valid
+      @reminder.errors.on(:before_units).should be_blank
+      @reminder.before_units.should == "days"
+      @reminder.errors.on(:before_value).should_not be_blank
+    end
+
+    it "should validate presence of delivery method" do
+      @reminder.should_not be_valid
+      @reminder.errors.on(:by_email).should_not be_blank      
+    end
+
+    it "should validate presence of rsvp conditions" do
+      @reminder.should_not be_valid
+      @reminder.errors.on(:to_yes).should_not be_blank      
+    end
+
+    it "should validate presence of send_reminder_at" do
+      @reminder.before_units = "days"
+      @reminder.before_value = -2
+      @reminder.should_not be_valid
+      @reminder.send_reminder_at.to_i.should == (@event.starting_at + 2.days).to_i
+      @reminder.errors.on(:before_value).should =~ /should be in a future/
+    end
+
+    it "should validate if sending by email" do
+      @reminder.by_email = true
+      @reminder.should_not be_valid
+      @reminder.errors.on(:email_subject).should_not be_blank
+      @reminder.errors.on(:email_body).should_not be_blank
+      @reminder.errors.on(:sms_message).should be_blank
+    end
+
+    it "should validate if sending by sms" do
+      @reminder.by_sms = true
+      @reminder.should_not be_valid
+      @reminder.errors.on(:email_subject).should be_blank
+      @reminder.errors.on(:email_body).should be_blank
+      @reminder.errors.on(:sms_message).should_not be_blank
+    end
   end
 
-  it "should create a new instance given valid attributes" do
-    Reminder.create!(@valid_attributes)
+  describe "should create healthy reminders" do
+    before(:each) do
+      @event = Factory.create(:event)
+    end
+  
+    [:to_yes, :to_no, :to_may_be, :to_not_responded].each do |whom_to|
+      [:by_email, :by_sms].each do |by|
+        it "should create when sending #{whom_to} #{by}" do
+          opts = {
+            :before_units => "days", :before_value => 2,
+          }
+          if :by_sms == by
+            opts[:sms_message] = "body body body"
+          else
+            opts[:email_body] = "body body body"
+            opts[:email_subject] = "subject subject subject"
+          end
+          opts[by] = true
+          opts[whom_to] = true
+
+          @reminder = @event.reminders.create(opts)
+          @reminder.should_not be_new_record
+        end
+      end
+    end
   end
 end
