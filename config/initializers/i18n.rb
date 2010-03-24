a86a926c0f8db1ad76aab291ab42db12cf64cbde
Boris Nadion <boris@astrails.com>
2010-03-24 13:05:04 +0200

    fix date time order for rtl layout

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -5,3 +5,18 @@ AVAILABLE_LOCALE_NAMES = {'en' => "English", 'he' => "עברית", 'ru' => "Ру
 FastGettext.add_text_domain 'app', :type => :db, :model => TranslationKey
 FastGettext.default_text_domain = 'app'
 FastGettext::TranslationRepository::Db.preload unless $0 =~ /rake/
+
+module ActionView
+  module Helpers
+    class DateTimeSelector
+      def build_selects_from_types_with_locale(order)
+        if "he" == current_controller.send(:current_locale)
+          build_selects_from_types_without_locale(order.reverse)
+        else
+          build_selects_from_types_without_locale(order)
+        end
+      end
+      alias_method_chain :build_selects_from_types, :locale
+    end
+  end
+end
