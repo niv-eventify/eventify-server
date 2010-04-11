b0f70697bd24427970a0740a0083646102f71045
Boris Nadion <boris@astrails.com>
2010-04-11 11:50:36 +0300

    js and lt7 from package 2 4

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,15 @@
 /*---- clear inputs ---*/
 function clearInputs(){
+	jQuery('input:text, input:password, textarea').each(function(){
+		var _el = jQuery(this);
+		var _val = _el.val();
+		_el.bind('focus', function(){
+			if(this.value == _val) this.value = '';
+		}).bind('blur', function(){
+			if(this.value == '') this.value = _val;
+		});
+	});
+	
 	jQuery('div.input-bg-alt > input').bind('focus', function(){
 		jQuery(this).parent().addClass('input-bg-alt-active');
 	}).bind('blur', function(){
@@ -22,8 +32,14 @@ function ieHover(h_list, h_class){
 	if(jQuery.browser.msie && jQuery.browser.version < 7){
 		if(!h_class) var h_class = 'hover';
 		jQuery(h_list).hover(function(){
+			if(jQuery(this).hasClass('img-box')){
+				jQuery(this).addClass('img-box-hover');
+			}
 			jQuery(this).addClass(h_class);
 		}, function(){
+			if(jQuery(this).hasClass('img-box-hover')){
+				jQuery(this).removeClass('img-box-hover');
+			}
 			jQuery(this).removeClass(h_class);
 		});
 	}
@@ -144,7 +160,7 @@ function initSlide(){
 	jQuery('div.edit').each(function(){
 		var _hold = jQuery(this);
 		var _t;
-		var _btn = _hold.find('a.open, a.open-edit');
+		var _btn = _hold.find('a.open');
 		var _box = _hold.find('div.drop');
 		if(_btn.length && _box.length){
 			_btn.click(function(){
@@ -211,7 +227,49 @@ function initSlide(){
 		}
 	});
 }
-
+/*--- promo navigation ---*/
+function promoNav(){
+	var _speed = 1000;
+	jQuery('div.promo div.promo-nav').each(function(){
+		var _hold = jQuery(this);
+		var btn_prev = _hold.find('a.prev');
+		var btn_next = _hold.find('a.next');
+		var list_hold = _hold.find('div.promo-nav-h > ul');
+		var _list = list_hold.children();
+		var list_w = 0;
+		for(var i = 0; i < _list.length; i++){ list_w += _list.eq(i).outerWidth();}
+		var hold_w = list_hold.parent().width();
+		var _step = hold_w;
+		var _m = 0;
+		if(list_w > hold_w){
+			btn_prev.click(function(){
+				moveList(false);
+				return false;
+			});
+			btn_next.click(function(){
+				moveList(true);
+				return false;
+			});
+		}
+		else{
+			btn_prev.click(function(){ return false;});
+			btn_next.click(function(){ return false;});
+		}
+		
+		function moveList(_f){
+			if(_f){
+				if(_m >= list_w - hold_w) _m = 0;
+				else _m += _step;
+			}
+			else{
+				if(_m > 0) _m -= _step;
+				else _m = Math.ceil((list_w-hold_w)/_step)*_step;
+			}
+			if(jQuery('body').hasClass('hebrew')) list_hold.animate({left:_m},{queue: false, duration: _speed});
+			else list_hold.animate({left:-_m},{queue: false, duration: _speed});
+		}
+	});
+}
 /*--- custom select ---*/
 jQuery.fn.customSelect = function(_options){
 var _options = jQuery.extend({
@@ -333,18 +391,44 @@ jQuery.fn.customCheckbox = function(_options){
 		else _this.get(0)._replaced.removeClass().addClass(_options.checkboxDefault);
 	}
 }
-
-jQuery.fn.redraw_customCheckbox = function(){
+jQuery.fn.redraw_customCheckbox = function() {
 	return this.each(function(){
 		var checkbox = jQuery(this);
 		if(checkbox.is(':checked')) checkbox.get(0)._replaced.removeClass().addClass("checkboxAreaChecked");
 		else checkbox.get(0)._replaced.removeClass().addClass("checkboxArea");
 	});
+};
+/*------------------------ SlideEffect -----------------------------*/
+function initSlideEffect(){
+    var _parentSlide = 'div.slider';
+    var _linkSlide = 'a.open-link';
+    var _slideBlock = 'div.slide';
+    var _openClassS = 'open-slide';
+    var _durationSlide = 500;
+    
+	if (jQuery(_parentSlide).hasClass('open-slide')) jQuery(_slideBlock).css('display','block');
+    jQuery(_parentSlide).each(function(){
+		if (!jQuery(this).is('.'+_openClassS)) {
+			jQuery(this).find(_slideBlock).css('display','none');
+		}
+    });
+    jQuery(_linkSlide,_parentSlide).click(function(){
+		if (jQuery(this).parents(_parentSlide).is('.'+_openClassS)) {
+			jQuery(this).parents(_parentSlide).removeClass(_openClassS);
+			jQuery(this).parents(_parentSlide).find(_slideBlock).slideUp(_durationSlide);
+		} else {
+			jQuery(this).parents(_parentSlide).addClass(_openClassS);
+			jQuery(this).parents(_parentSlide).find(_slideBlock).slideDown(_durationSlide);
+		}
+		return false;
+    });
 }
-
 jQuery(document).ready(function(){
-	ieHover('div.img-box,.edit-bg');
+	initSlideEffect();
+	ieHover('.img-box, .edit-bg, .subnav li');
 	clearInputs();
 	initSlide();
+	promoNav();
+	jQuery('select').customSelect();
 	jQuery('input:checkbox').customCheckbox();
 });
