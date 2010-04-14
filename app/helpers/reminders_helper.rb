2c1df855b5ac9803acee3fafe7ced94a735a2f49
Boris Nadion <boris@astrails.com>
2010-04-14 16:55:41 +0300

    reminders UI

--- app/helpers/reminders_helper.rb
+++ app/helpers/reminders_helper.rb
@@ -8,15 +8,10 @@ module RemindersHelper
     end
   end
 
-  def reminder_info(reminder)
-    res = "#{reminder.before_in_words}"
-    res << " "  << s_("reminder|(sent)") unless reminder.reminder_sent_at.nil?
-    res
-  end
-
   def reminder_remote_is_active(reminder)
+    return unless reminder.reminder_sent_at.nil?
     form_remote_for :reminder, reminder, :url => event_reminder_path(reminder.event_id, reminder), :method => :put do |f|
-      haml_concat f.check_box(:is_active, :onchange => "jQuery(this).parents('form').get(0).onsubmit()", :id => "#{dom_id(reminder)}_is_active", :disabled => !reminder.reminder_sent_at.nil?)
+      haml_concat f.check_box(:is_active, :onchange => "jQuery(this).parents('form').get(0).onsubmit()", :id => "#{dom_id(reminder)}_is_active")
     end
   end
 
--- lib/inline_error_form_builder.rb
+++ lib/inline_error_form_builder.rb
@@ -1,9 +1,9 @@
 module InlineErrorFormBuilder
   class Builder < ActionView::Helpers::FormBuilder
     def inline_errors_for(method)
-      return "" unless has_errors?(method)
-
-      @template.content_tag(:p, merge_errors(method), :class => "error-msg")
+      has_errors?(method) ?
+        @template.content_tag(:p, merge_errors(method), :class => "error-msg") :
+        @template.content_tag(:p, "&nbsp;", :class => "error-msg")
     end
 
     include CommonForm
