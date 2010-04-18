a8e3bb0b1cd2c9c0c294d29980266dd69c058b0d
Boris Nadion <boris@astrails.com>
2010-04-18 14:40:02 +0300

    managing contacts ui

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,2 +1,19 @@
 module ContactsHelper
+  SUBTABS = [
+    {:id => :index, :url => "/contacts", :title => N_("contacts tab|All Contacts")},
+    {:id => :import, :url => "/contact_importers", :title => N_("contacts tab|Import")},
+    {:id => :new, :url => "/contacts/new", :title => N_("contacts tab|Add Contact")}
+  ]
+
+  def contacts_navigation(tab_id)
+    haml_tag(:div, :class => "nav-bar") do
+      haml_tag(:ul) do
+        SUBTABS.each do |t|
+          haml_tag(:li, :class => (tab_id == t[:id] ? "active" : "")) do
+            haml_concat link_to(s_(t[:title]), t[:url])
+          end          
+        end
+      end
+    end
+  end
 end
