5bcaef3fa081d1bae786d4c117238ff5cfc9e6f9
Niv Caner <niv@Kiba1.(none)>
2010-04-25 20:42:27 +0300

    change lightbox size in rsvp

--- public/javascripts/rsvps.js
+++ public/javascripts/rsvps.js
@@ -4,11 +4,11 @@
 
 	adjust_dialog_size: function() {
 		var width = rsvps.win_width - 62;
-		var height = rsvps.win_height - 83;
+		var height = rsvps.win_height - 63;
 
 		if($("#background_holder").attr("strech") == "true") {
-			$("#background_holder, #invitation").css("width",width + "px");
-			$("#background_holder, #invitation").css("height",height + "px");
+			$("#background_holder, #invitation").css("width",(width - 60) + "px");
+			$("#background_holder, #invitation").css("height",(height - 30) + "px");
 		} else {
 			var ratio = width / height;
 			if(ratio < 1.5) {
@@ -29,6 +29,7 @@ $(document).ready(function(){
 	rsvps.win_width = $(window).width();
 	rsvps.adjust_dialog_size();
 	if($(".envelope").length > 0) {
+		$("a.nyroModal").nyroModal({closeButton:''});
 		setTimeout(function(){
 			$(".envelope").click();
 			$("#toolbar").show();
