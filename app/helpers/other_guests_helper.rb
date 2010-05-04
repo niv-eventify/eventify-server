bae659cc0dfb34f9a0d6dfb7f7e3e7c151a38f74
Boris Nadion <boris@astrails.com>
2010-05-04 19:59:03 +0300

    some ui fixes

--- app/helpers/other_guests_helper.rb
+++ app/helpers/other_guests_helper.rb
@@ -1,2 +1,14 @@
 module OtherGuestsHelper
+  def tab_dom_id
+    @tab_dom_id ||= case params[:filter]
+    when "no"
+      "tab-3"
+    when "maybe"
+      "tab-2"
+    when "not_responded"
+      "tab-4"
+    else
+      "tab-1"
+    end
+  end
 end
