24edd4d2827150c3f10b2d5efb395def9febd417
Boris Nadion <boris@astrails.com>
2010-05-17 00:35:33 +0300

    fix summaries controller

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,10 +1,14 @@
 class SummariesController < InheritedResources::Base
   defaults :resource_class => Event, :collection_name => 'events', :instance_name => 'event', :route_instance_name => "summary"
-  before_filter :require_user
+  before_filter :require_user, :check_invitations
   actions :show
 
 protected
   def begin_of_association_chain
     current_user
+  end
+
+  def check_invitations
+    return redirect_to(edit_invitation_path(resource)) unless resource.invitations_to_send_counts[:total].zero?
   end
 end
