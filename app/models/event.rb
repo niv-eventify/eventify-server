4e8ecbcb2ce89e4c55730f15234bc30fd06724ae
Boris Nadion <boris@astrails.com>
2010-05-25 00:37:18 +0300

    import from eventify contacts

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,20 @@ class Event < ActiveRecord::Base
   accepts_nested_attributes_for :user
   validates_associated :user, :if => proc { |e| e.user.activated_at.blank? }
 
-  has_many :guests
+  has_many :guests do
+    def import(new_guests)
+      guests_imported = 0
+
+      new_guests.each do |g|
+        guest = build(:name => g[:name], :email => g[:email], :mobile_phone => g[:mobile])
+        guest.send_email = true unless guest.email.blank?
+        guest.send_sms = true unless guest.mobile_phone.blank?
+        guests_imported += 1 if guest.save
+      end
+
+      guests_imported
+    end
+  end
   has_many :things do
     def total_amount
       calculate(:sum, :amount)
