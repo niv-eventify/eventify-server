e2577a430d1ca85c18ffa9db1818ac90206aee18
niv <niv@eventify.co.il>
2010-06-01 14:19:15 +0300

    make carousel get data from xml via httpservice

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
     @popular_categories = Category.enabled.popular(2)
+    respond_to do |format|
+      format.html {logger.debug('html')}
+      format.xml { render :template => "designs/carousel", :layout => false }
+    end
   end
 end
