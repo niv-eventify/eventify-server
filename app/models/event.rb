60686d2ad8298e1553f55457dbb6b7492461a28c
Boris Nadion <boris@astrails.com>
2010-06-03 11:33:20 +0300

    default sms message

--- app/models/event.rb
+++ app/models/event.rb
@@ -215,9 +215,9 @@ class Event < ActiveRecord::Base
         :host_name => user.name,
         :date => starting_at.to_s(:isra_date),
         :time => starting_at.to_s(:isra_time),
-        :location => (location.blank? ? "" : location)
+        :location => (location.blank? ? "" : (_("at location %{location}") % {:location => location}))
       }
-      s = _("%{event_name} on %{date} at %{time}%{location}. %{host_name}") % opts
+      s = _("%{event_name} on %{date} at %{time}%{location}. Invite sent to your email. %{host_name}") % opts
       return s if s.length < SmsMessage::MAX_LENGTH # check sms length
 
       _("%{event_name} on %{date} at %{time}. %{host_name}") % opts
