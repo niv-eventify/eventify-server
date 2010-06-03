c8ae77e80a84447cc9c4f190e4e691f6c8c7b635
Boris Nadion <boris@astrails.com>
2010-06-03 14:07:33 +0300

    import guests flow

--- app/controllers/guest_importers_controller.rb
+++ app/controllers/guest_importers_controller.rb
@@ -10,22 +10,23 @@ class GuestImportersController < ApplicationController
   }
 
   def create
-    if params[:source]
-      set_source
-      if "csv" == params[:source]
-        _load_csv_file
-        render :partial => "preview", :layout => false
-      elsif "email" == params[:source]
-        _load_from_emails
-        render(:update) do |page|
-          page << "jQuery.nyroModalManual({content:#{render(:partial => "import", :locals => {:title => s_(GuestImportersController::TITLES[@source]), :contacts => @contacts}).to_json}})"
-        end
-      end
-      
-    else
+    unless params[:source]
       guests_imported = @event.guests.import(selected_contracts)
       flash[:notice] = n_("%d guest imported", "%d guests imported", guests_imported) % guests_imported
       redirect_to event_guests_path(@event)
+      return
+    end
+
+    _import
+
+    responds_to_parent do
+      render(:update) do |page|
+        if @error
+          page << "jQuery.nyroModalManual({content: #{render(:partial => @source).to_json}});"
+        else
+          page << "jQuery.nyroModalManual({content:#{render(:partial => "import", :locals => {:title => s_(GuestImportersController::TITLES[@source]), :contacts => @contacts}).to_json}})"
+        end
+      end
     end
   end
 
@@ -55,15 +56,27 @@ protected
 
     unless params[:csvfile].blank?
       contacts, error = ContactImporter.import_contacts(nil, nil, "csv", params[:csvfile])
-      @contacts = ContactImporter.contacts_to_openstruct(contacts) if contacts
+      @contacts = ContactImporter.contacts_to_openstruct(contacts) if contacts && contacts.is_a?(Array)
       @error = error.to_s if error
+    else
+      @error = _("Please upload a CSV file")
     end
   end
 
   def _load_from_emails
     @contacts = []
     contacts, error = ContactImporter.import_contacts(params[:username], params[:password], params[:contact_source], nil)
-    @contacts = ContactImporter.contacts_to_openstruct(contacts) if contacts
+    @contacts = ContactImporter.contacts_to_openstruct(contacts) if contacts && contacts.is_a?(Array)
     @error = error.to_s if error
+  end
+
+  def _import
+    set_source
+
+    if "csv" == params[:source]
+      _load_csv_file
+    elsif "email" == params[:source]
+      _load_from_emails
+    end
   end
 end
--- app/helpers/guest_importers_helper.rb
+++ app/helpers/guest_importers_helper.rb
@@ -1,13 +1,2 @@
 module GuestImportersHelper
-  def render_in_parent(opts)
-    haml_tag :html, :xmlns => "http://www.w3.org/1999/xhtml" do
-      haml_tag :head do
-        haml_tag :meta, :content => "text/html; charset=utf-8", "http-equiv" => "Content-Type"
-        haml_tag :meta, "http-equiv" => "X-UA-Compatible", :content => "IE=EmulateIE7"
-      end
-      haml_tag :body do
-        haml_concat javascript_tag("window.parent.jQuery.nyroModalManual({content:#{render(opts).to_json}})")
-      end
-    end
-  end
 end
