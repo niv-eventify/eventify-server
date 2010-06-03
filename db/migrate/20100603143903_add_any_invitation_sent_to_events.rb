e0b121a45dd8d3ae4822a9b1dc3311c54d45bbb3
Boris Nadion <boris@astrails.com>
2010-06-03 21:02:22 +0300

    fix migraron

--- db/migrate/20100603143903_add_any_invitation_sent_to_events.rb
+++ db/migrate/20100603143903_add_any_invitation_sent_to_events.rb
@@ -1,6 +1,6 @@
 class AddAnyInvitationSentToEvents < ActiveRecord::Migration
   def self.up
-    # add_column :events, :any_invitation_sent, :boolean, :default => false
+    add_column :events, :any_invitation_sent, :boolean, :default => false
     ActiveRecord::Base.connection.execute "update events set any_invitation_sent = 1 where stage_passed = 4"
   end
 
