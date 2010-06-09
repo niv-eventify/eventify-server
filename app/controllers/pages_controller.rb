57fe0f05aa3af35dbd4beb8dddbdc800c45b2f95
Boris Nadion <boris@astrails.com>
2010-06-09 10:57:13 +0300

    fix static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < HighVoltage::PagesController
 
 protected
   def current_page
-    "pages/#{params[:id].to_s.downcase}.#{current_locale}"
+    "pages/#{params[:id].to_s.downcase}_#{current_locale}"
   end
 
 end
