3378cb025c4f15010dacaaba92ec4f4cf8cdd022
niv <niv@eventify.co.il>
2010-06-10 23:12:30 +0300

    show window in stage 2 and admin area

--- app/helpers/windows_helper.rb
+++ app/helpers/windows_helper.rb
@@ -1,2 +1,5 @@
 module WindowsHelper
+  def window_css(window, ratio = 1)
+    window.window_dimensions(ratio).keys.map {|k| "#{k}:#{window.window_dimensions(ratio)[k]}"}.join(";")
+  end
 end
--- app/models/window.rb
+++ app/models/window.rb
@@ -5,4 +5,13 @@ class Window < ActiveRecord::Base
   validates_numericality_of :top_x, :top_y, :width, :height
 
   attr_accessible :design_id, :top_x, :top_y, :width, :height
+
+  def window_dimensions(ratio)
+    @window_dimensions ||= {
+      :top =>    "#{(top_y/ratio).to_int}px",
+      :left =>   "#{(top_x/ratio).to_int}px",
+      :width =>  "#{(width/ratio).to_int}px",
+      :height => "#{(height/ratio).to_int}px"
+    }
+  end
 end
