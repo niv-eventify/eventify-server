2110bd5eec1835546426d31fa1a083bfc8a67e69
Boris Nadion <boris@astrails.com>
2010-06-15 17:12:38 +0300

    smart login redirect

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,8 @@ class WelcomeController < ApplicationController
   def index
     @popular_categories = Category.enabled.popular(2)
     @designs = Design.carousel
+    @redirect_on_login = true
+
     respond_to do |format|
       format.html {logger.debug('html')}
       format.xml { render :template => "designs/carousel", :layout => false }
