375cd124d0f025825ff2cb1826732f5fc115a073
niv <niv@eventify.co.il>
2010-07-05 11:49:05 +0300

    fix jquery selector in livevalidations

--- config/initializers/live_validations.rb
+++ config/initializers/live_validations.rb
@@ -1,7 +1,7 @@
 LiveValidations.use :jquery_validations, :default_valid_message => "", :validate_on_blur => true, 
   :validator_settings => {
     :errorElement => "p", :errorClass => "error-msg",
-    :highlight => "function(element, errorClass){jQuery(element).parent('li').addClass('error')}",
-    :unhighlight => "function(element, errorClass){jQuery(element).parent('li').removeClass('error')}",
+    :highlight => "function(element, errorClass){jQuery(element).parents('li:first').addClass('error')}",
+    :unhighlight => "function(element, errorClass){jQuery(element).parents('li:first').removeClass('error')}",
     :errorPlacement => "function(error, element){jQuery(element).parents('.input-bg-alt,.textarea-bg').after(error);}"
   }
