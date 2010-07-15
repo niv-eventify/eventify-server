c036cd5d9c051cbf1016600b8f2bdee5d130f593
niv <niv@eventify.co.il>
2010-07-15 21:56:37 +0300

    allow hebrew snippet strings to be as big as english snippets

--- config/initializers/urf_split.rb
+++ config/initializers/urf_split.rb
@@ -1,28 +1,9 @@
 # http://talklikeaduck.denhaven2.com/2009/05/28/safely-dividing-a-utf-8-string-in-ruby
 
 class String
-  def valid_utf8?
-    unpack("U") rescue nil
-  end
-
-  def utf8_safe_split(n)
-    if length <= n
-      [self, nil]
-    else
-      before = self[0, n]
-      after = self[n..-1]
-      until after.valid_utf8?
-        n = n - 1
-        before = self[0, n]
-        after = self[n..-1]
-      end      
-      [before, after.empty? ? nil : after]
-    end
-  end  
-
-  def utf_snippet(len = 20)
-    if len < self.length
-      "#{self.utf8_safe_split(len).first}..."
+  def utf_snippet(len = 22)
+    if len < self.mb_chars.length
+      "#{self.mb_chars[0,len]}..."
     else
       self
     end
