7aabad2680e3255288eb643ec1a57d9161fefea8
niv <niv@eventify.co.il>
2010-09-02 16:06:09 +0300

    after live validations fail, return cursor to default appearance

--- config/initializers/live_validations.rb
+++ config/initializers/live_validations.rb
@@ -3,5 +3,5 @@ LiveValidations.use :jquery_validations, :default_valid_message => "", :validate
     :errorElement => "p", :errorClass => "error-msg",
     :highlight => "function(element, errorClass){jQuery(element).parents('li:first').addClass('error')}",
     :unhighlight => "function(element, errorClass){jQuery(element).parents('li:first').removeClass('error')}",
-    :errorPlacement => "function(error, element){jQuery(element).parents('.input-bg-alt,.textarea-bg').after(error);}"
+    :errorPlacement => "function(error, element){jQuery(element).parents('.input-bg-alt,.textarea-bg').after(error);jQuery('body').css('cursor', 'default');}"
   }
