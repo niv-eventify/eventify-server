84425542606b37acc2e207831fe71f2006b9a01f
Vitaly Gorodetsky <gorodetsky@astrails.com>
2010-09-27 22:06:28 +0200

    specs and updates

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -25,6 +25,10 @@ class RemindersController < InheritedResources::Base
   # destroy
 
 protected
+  def collection
+    get_collection_ivar || set_collection_ivar(end_of_association_chain.find(:all))
+  end
+
   def begin_of_association_chain
     current_user
   end
