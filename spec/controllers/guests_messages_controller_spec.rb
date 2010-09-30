b15f917efb3d7cbe5837bd55327da711e708a4ec
Vitaly Gorodetsky <gorodetsky@astrails.com>
2010-09-30 15:39:37 +0200

    guests messages specs

--- spec/controllers/guests_messages_controller_spec.rb
+++ spec/controllers/guests_messages_controller_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe GuestsMessagesController do
+  setup :activate_authlogic
+  integrate_views
 
-  #Delete this example and add some real ones
-  it "should use GuestsMessagesController" do
-    controller.should be_an_instance_of(GuestsMessagesController)
+  before(:each) do
+    @event = Factory.create(:event)
+    UserSession.create(@event.user)
   end
+  
+  it "should render new" do
+    xhr :get, :new, :event_id => @event.id
+    response.should be_success
+    response.should render_template(:new)
+  end
+
+  it "should create guests message" do
+    lambda {
+      xhr :post, :create, :event_id => @event.id, :guests_message => {:subject => "some subject", :body => "some body"}
+    }.should change(GuestsMessage, :count).by(1)
+    response.should be_success
+  end  
 
 end
