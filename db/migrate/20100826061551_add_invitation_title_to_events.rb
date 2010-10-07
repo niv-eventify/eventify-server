10f618cc30a5d6ba39aed56e219a560052c7f5dd
Vitaly Gorodetsky <gorodetsky@astrails.com>
2010-10-07 11:53:47 +0200

    fixed migration to work

--- db/migrate/20100826061551_add_invitation_title_to_events.rb
+++ db/migrate/20100826061551_add_invitation_title_to_events.rb
@@ -1,8 +1,8 @@
 class AddInvitationTitleToEvents < ActiveRecord::Migration
   def self.up
     add_column :events, :invitation_title, :string, :limit => 100
+    Event.update_all "invitation_title = name"
   end
-  Event.update_all "invitation_title = name"
 
   def self.down
     remove_column :events, :invitation_title
