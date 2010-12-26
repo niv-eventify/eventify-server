a4acd7a16452345286ef9bc389d7660f75e6c960
niv <niv@eventify.co.il>
2010-12-26 14:54:49 +0200

    fix cookie for google analytics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < HighVoltage::PagesController
 
   skip_before_filter :checkie6
-
+  layout :pages_layout
 protected
   def current_page
     "pages/#{params[:id].to_s.downcase}_#{current_locale}"
   end
-
+private
+  def pages_layout
+    params[:id].to_s.downcase == "analytics_cookie" ? false : "application"
+  end
 end
