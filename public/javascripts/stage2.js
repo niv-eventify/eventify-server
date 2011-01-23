0abc285a8c15523eb82b7f94005e4a9aa61ec5ef
niv <niv@eventify.co.il>
2011-01-23 13:26:45 +0200

    add form focus and change feedback for analytics in stage2 take 3

--- public/javascripts/stage2.js
+++ public/javascripts/stage2.js
@@ -490,9 +490,9 @@ jQuery(document).ready(function(){
     zIndex: 200
   });
   jQuery("input,textarea").focus(function(){
-    _gaq.push(['_trackEvent', 'Forms', 'New Event', this.name, 'focus']);
+    _gaq.push(['_trackEvent', 'New Event', this.name, 'focus']);
   });
   jQuery("input,textarea").change(function(){
-    _gaq.push(['_trackEvent', 'Forms', 'New Event', this.name, 'change']);
+    _gaq.push(['_trackEvent', 'New Event', this.name, 'change']);
   });
 });
