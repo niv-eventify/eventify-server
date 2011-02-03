9b9f01bd4992c625f82fd10b0d00030e2a499631
niv <niv@eventify.co.il>
2011-02-03 15:55:28 +0200

    oops

---
+++ lib/astrails/email_list.rb
@@ -0,0 +1,32 @@
+require 'logger'
+module Astrails
+  class EmailList
+    class << self
+      def get(email_list)
+        return nil if email_list.blank?
+        email_list = email_list.gsub(/</, "").gsub(/>/,"").gsub(/\r/, "").gsub(/\n/, "").gsub(/\t/, "")
+        data_arr = email_list.split(/[,|;]/)
+        res = {:contacts => [], :no_mails => []}
+        0.step(data_arr.length - 1) do |index|
+          contact = data_arr[index].split(/ /)
+          name = email = ""
+          0.step(contact.length - 1) do |jindex|
+            if String::EMAIL_INSIDE_REGEX.match(contact[jindex]) == nil
+              name += contact[jindex] + " "
+            else
+              email = $&
+            end
+          end
+          if email.blank?
+            res[:no_mails] << name
+            next
+          end
+          name = name.strip
+          name = email if name.blank?
+          res[:contacts] << [name, email]
+        end
+        res          
+      end
+    end
+  end
+end
