ed452bdf77c9d6019f28c6a9528125112e363309
niv <niv@eventify.co.il>
2011-02-23 12:24:33 +0200

    seperate msg and title in designs

---
+++ db/migrate/20110223101756_seperate_msg_and_title_in_designs.rb
@@ -0,0 +1,35 @@
+class SeperateMsgAndTitleInDesigns < ActiveRecord::Migration
+  def self.up
+    Design.find(:all).each do |d|
+      unless d.is_seperated_title?
+        old_text_top_x = d.text_top_x
+        old_text_top_y = d.text_top_y
+        old_text_width = d.text_width
+        old_text_height = d.text_height
+        d.title_top_x = old_text_top_x
+        d.title_top_y = old_text_top_y
+        d.title_width = old_text_width
+        d.title_height = (old_text_height.to_f / 3).ceil
+        d.text_top_x = old_text_top_x
+        d.text_top_y = old_text_top_y + ((old_text_height.to_f / 3).ceil) + 5
+        d.text_width = old_text_width
+        d.text_height = (old_text_height.to_f * 2 / 3).ceil
+        d.save
+      end
+    end
+  end
+
+  def self.down
+    Design.find(:all).each do |d|
+      unless d.text_width != d.title_width || d.title_top_x != d.text_top_x
+        d.text_top_y = d.title_top_y
+        d.text_height = d.title_height * 3
+        d.title_top_x = nil
+        d.title_top_y = nil
+        d.title_width = nil
+        d.title_height = nil
+        d.save
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110221100325) do
+ActiveRecord::Schema.define(:version => 20110223101756) do
 
   create_table "categories", :force => true do |t|
     t.string   "name_en"
