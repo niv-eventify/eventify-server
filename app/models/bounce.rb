6a0b349e81e1622f934b925420da286f301cc977
Niv Caner <nivcaner@gmail.com>
2011-05-13 14:38:58 +0300

    fix bounce cron job

--- app/models/bounce.rb
+++ app/models/bounce.rb
@@ -1,17 +1,18 @@
 class Bounce
 
   include HTTParty
-  
+  require 'hpricot'
+
   def self.all
     res = get("https://sendgrid.com/api/bounces.get.xml?api_user=#{ActionMailer::Base.smtp_settings[:user_name]}&api_key=#{ActionMailer::Base.smtp_settings[:password]}")
-    res && res["bounces"] && res["bounces"]["bounce"]
   end
 
   def self.cron_job
     begin
-      (all || []).each do |bounce|
-        if g = Guest.not_bounced_by_email(bounce["email"]).first
-          g.bounce!(bounce["status"], bounce["reason"])
+      doc = Hpricot::XML(all)
+      (doc/:bounce || []).each do |bounce|
+        if g = Guest.not_bounced_by_email(bounce.at("email").innerHTML).first
+          g.bounce!(bounce.at("status").innerHTML, bounce.at("reason").innerHTML)
         end
       end
     rescue => e
