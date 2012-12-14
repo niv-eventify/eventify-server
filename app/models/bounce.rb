4fd61c90aa15868621a118c1d580e9905180f577
Niv Caner <nivcaner@gmail.com>
2012-12-14 20:35:48 +0200

    add logger info to bounce model

--- app/models/bounce.rb
+++ app/models/bounce.rb
@@ -5,9 +5,13 @@ class Bounce
 
   def self.blocks
     res = get("https://sendgrid.com/api/blocks.get.xml?api_user=#{ActionMailer::Base.smtp_settings[:user_name]}&api_key=#{ActionMailer::Base.smtp_settings[:password]}")
+    logger.info("blocks: #{res}")
+    return res
   end
   def self.all
     res = get("https://sendgrid.com/api/bounces.get.xml?api_user=#{ActionMailer::Base.smtp_settings[:user_name]}&api_key=#{ActionMailer::Base.smtp_settings[:password]}")
+    logger.info("bounces: #{res}")
+    return res
   end
 
   def self.cron_job
@@ -18,6 +22,7 @@ class Bounce
           g.bounce!(bounce.at("status").innerHTML, "bounced: #{bounce.at('reason').innerHTML}")
         end
       end
+      debugger
       self.blocks.parsed_response['blocks']['block'].each do |block|
         if g = Guest.not_bounced_by_email(block["email"]).first
           g.bounce!(block["status"], "blocked: #{block['reason']}")
