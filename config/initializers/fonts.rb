bb5d58b90c7fcb2632498222cc411b6153d9208e
Niv Caner <nivcaner@gmail.com>
2012-12-14 21:19:38 +0200

    try to allow pdf to use fonts take 2

--- config/initializers/fonts.rb
+++ config/initializers/fonts.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   font_dir = File.join(Dir.home, ".fonts")
   Dir.mkdir(font_dir) unless Dir.exists?(font_dir)
 
-  Dir.glob(Rails.root.join("vendor", "fonts", "*")).each do |font|
+  Dir.glob(Rails.root.join("public", "fonts", "*")).each do |font|
     target = File.join(font_dir, File.basename(font))
     File.symlink(font, target) unless File.exists?(target)
   end
