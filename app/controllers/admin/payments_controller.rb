c70a1bcc6ecde422ea4bf0bed71e7881b545ff49
Niv Caner <nivcaner@gmail.com>
2012-12-26 21:13:19 +0200

    order admin/payments correctly

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -2,6 +2,7 @@ class Admin::PaymentsController < InheritedResources::Base
 
   before_filter :require_admin
   has_scope :for_list, :default => true
+  has_scope :paid, :default => true
 
   actions :index
 
