ed437a078330aa24ce164bee680fba0407783a5c
Niv Caner <nivcaner@gmail.com>
2013-03-18 22:25:07 +0200

    remove uniqueness from links_page friendly_url

--- app/models/links_page.rb
+++ app/models/links_page.rb
@@ -1,5 +1,5 @@
 class LinksPage < ActiveRecord::Base
   validates_presence_of :language, :friendly_url, :page_html
-  validates_uniqueness_of :friendly_url, :message => "must be unique"
+  #validates_uniqueness_of :friendly_url, :message => "must be unique"
   attr_accessible  :language, :friendly_url, :title, :meta_description, :meta_keywords, :page_html
 end
